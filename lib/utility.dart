44fad908e441ef12cf93189f5ad672d1e538eca4
RadXGH <vradnisntlong@gmail.com>
2023-09-27 01:46:19 +0700

    Added mobile and dekstop configurations

--- lib/utility.dart
+++ lib/utility.dart
@@ -1,3 +1,18 @@
+class Mobile {
+  static double minScreenWidth = 750.0;
+  static double fontSize = 12.0;
+
+  // grid view related
+  static double childAspectRatio = (1.2 / 1.5);
+}
+
+class Dekstop {
+  static double fontSize = 18.0;
+
+  // grid view related
+  static double childAspectRatio = (2 / 4);
+}
+
 class JikanApi {
   static String baseUrl = 'https://api.jikan.moe/v4';
   static String animeSearch = '/anime'; // https://api.jikan.moe/v4/anime[?page=1&q=str ...etc]
