28e591e39138a167c1cfa5b93f24e0563bfb85fe
RadXGH <vradnisntlong@gmail.com>
2023-09-28 23:43:25 +0700

    Changed aspect ratio

--- lib/utility.dart
+++ lib/utility.dart
@@ -1,17 +1,21 @@
+double fontSizeLargerOffset = 4.0;
+
 class Mobile {
   static double minScreenWidth = 750.0;
   static double fontSize = 12.0;
 
   // grid view related
-  static double childAspectRatio = (1.2 / 1.5);
+  static double childAspectRatio = (1 / 1.5);
 }
 
 class Dekstop {
   static double fontSize = 18.0;
 
   // grid view related
-  static double childAspectRatio = (2 / 4);
+  static double childAspectRatio = (2 / 3);
 }
+
+// var cardSize = screenSize.width <= Mobile.minScreenWidth ? {'height': 215.0, 'width': 125.0} : {'height': 315.0, 'width': 225.0};
 
 class JikanApi {
   static String baseUrl = 'https://api.jikan.moe/v4';
