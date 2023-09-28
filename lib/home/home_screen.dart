d5431c0dca2b0b1733899ea10082d80f0a08808a
RadXGH <vradnisntlong@gmail.com>
2023-09-28 23:44:00 +0700

    Fixed how the list is showed

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -57,7 +57,7 @@ class SearchTextField extends StatelessWidget {
         autocorrect: false,
         decoration: InputDecoration(
             border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10.0)
+              borderRadius: BorderRadius.circular(20.0)
             ),
             hintText: 'Search by anime title here...'
           ),
@@ -77,55 +77,55 @@ class SearchResultListState extends State<SearchResultList> {
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
-    var cardSize = screenSize.width <= Mobile.minScreenWidth ? {'height': 215.0, 'width': 125.0} : {'height': 315.0, 'width': 225.0};
-    return FutureBuilder<Map?>(
-      future: Anime.getSearchResult(widget.query, '1'),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          if (snapshot.hasData) {
-            if (snapshot.data!.isNotEmpty) {
-              List data = snapshot.data!.containsKey('data') ? snapshot.data!['data'] : null;
-              // Map pagination = snapshot.data!.containsKey('pagination') ? snapshot.data!['pagination'] : null;
-              return GridView.builder(
-                padding: EdgeInsets.zero,
-                shrinkWrap: true,
-                itemCount: data.length,
-                physics: const BouncingScrollPhysics(
-                  parent: AlwaysScrollableScrollPhysics()
-                ),
-                gridDelegate: screenSize.width <= Mobile.minScreenWidth ? 
-                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, mainAxisSpacing: 5, childAspectRatio: Mobile.childAspectRatio) : 
-                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, mainAxisSpacing: 5, childAspectRatio: Dekstop.childAspectRatio),
-                itemBuilder: (_, index) {
-                  Map image = data[index]['images'].containsKey('webp') ? data[index]['images']['webp'] : data[index]['images']['jpg'];
-                  Map titles = {
-                    'jpTitle': data[index]['title'],
-                    'enTitle': data[index]['title_english'] ?? ''
-                  };
-                  String id = data[index]['mal_id'].toString();
-                  return Align(
-                    alignment: Alignment.center,
-                    child: SizedBox(
-                      height: cardSize['height']!,
-                      width: cardSize['width']!,
-                      child: ThumbnailCard(dataId: id, dataImage: image['image_url'], dataTitles: titles),
-                    ),
-                  );
-                },
-              );
-            }
-            else if (snapshot.data!.isEmpty) {
-              return const Center(
-                child: Text('No Result'),
-              );
+    return Container(
+      margin: const EdgeInsets.all(15.0),
+      padding: const EdgeInsets.all(3.0),
+      width: screenSize.width <= Mobile.minScreenWidth ? 450.0 : 750.0,
+      // decoration: BoxDecoration(
+      //   border: Border.all(color: const Color.fromARGB(255, 61, 61, 61))
+      // ),
+      child: FutureBuilder<Map?>(
+        future: Anime.getSearchResult(widget.query, '1'),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              if (snapshot.data!.isNotEmpty) {
+                List data = snapshot.data!.containsKey('data') ? snapshot.data!['data'] : null;
+                // Map pagination = snapshot.data!.containsKey('pagination') ? snapshot.data!['pagination'] : null;
+                return GridView.builder(
+                  padding: EdgeInsets.zero,
+                  shrinkWrap: true,
+                  itemCount: data.length,
+                  physics: const BouncingScrollPhysics(
+                    parent: AlwaysScrollableScrollPhysics()
+                  ),
+                  gridDelegate: screenSize.width <= Mobile.minScreenWidth ? 
+                    SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, mainAxisSpacing: 5, crossAxisSpacing: 5, childAspectRatio: Mobile.childAspectRatio) : 
+                    SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, mainAxisSpacing: 10, crossAxisSpacing: 10, childAspectRatio: Dekstop.childAspectRatio),
+                  itemBuilder: (_, index) {
+                    Map image = data[index]['images'].containsKey('webp') ? data[index]['images']['webp'] : data[index]['images']['jpg'];
+                    Map titles = {
+                      'jpTitle': data[index]['title'],
+                      'enTitle': data[index]['title_english'] ?? ''
+                    };
+                    String id = data[index]['mal_id'].toString();
+                    return ThumbnailCard(dataId: id, dataImage: image['image_url'], dataTitles: titles);
+                  },
+                );
+              }
+              else if (snapshot.data!.isEmpty) {
+                return const Center(
+                  child: Text('No Result'),
+                );
+              }
             }
           }
-        }
-        else if (snapshot.hasError) {
-          return Text(snapshot.error.toString());
-        }
-        return const Center(child: CircularProgressIndicator());
-      },
+          else if (snapshot.hasError) {
+            return Text(snapshot.error.toString());
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
     );
   }
 }
@@ -139,7 +139,31 @@ class ThumbnailCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
+    String dialogTitle = 'Details';
+
+    showDetailModal() {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            contentPadding: EdgeInsets.only(top: 10.0),
+            title: screenSize.width <= Mobile.minScreenWidth ?
+              Text(dialogTitle, style: TextStyle(fontSize: Mobile.fontSize),) :
+              Text(dialogTitle, style: TextStyle(fontSize: Dekstop.fontSize + fontSizeLargerOffset),),
+            content: SizedBox(
+              height: 300.0,
+              width: 500.0,
+              child: SearchThemeScreen(dataId: dataId),
+            ),
+          );
+        }
+      );
+    }
+    
     return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0)
+      ),
       elevation: 0,
       semanticContainer: true,
       clipBehavior: Clip.antiAliasWithSaveLayer,
@@ -148,7 +172,8 @@ class ThumbnailCard extends StatelessWidget {
         fit: BoxFit.cover,
         child: InkWell(
           onTap: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) => SearchThemeScreen(dataId: dataId)));
+            Navigator.push(context, MaterialPageRoute(builder: (context) => Scaffold(body: SearchThemeScreen(dataId: dataId),)));
+            // showDetailModal();
           },
           child: Stack(
             children: [
