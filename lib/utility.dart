8f61251d93ccfb2e6d34ed38ebf604dd032bb4ef
RadXGH <vradnisntlong@gmail.com>
2023-10-03 00:23:53 +0700

    Added YouTube URL in a new CommonURL class

--- lib/utility.dart
+++ lib/utility.dart
@@ -22,3 +22,7 @@ class JikanApi {
   static String animeSearch = '/anime'; // https://api.jikan.moe/v4/anime[?page=1&q=str ...etc]
   static String themes = '/themes'; // https://api.jikan.moe/v4/anime/{id}/themes
 }
+
+class CommonUrl {
+  static String youtube = 'https://www.youtube.com';
+}
